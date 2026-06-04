ff610aa81b4eb2441a651a1ee024e1b4ee845af2
Howard Gil <howardbgil@gmail.com>
2026-06-04 12:28:56 -0700

    init

---
+++ TakeMyPic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TakeMyPic
+//
+//  Created by Howard Gil on 6/4/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TakeMyPic/TakeMyPicApp.swift
@@ -0,0 +1,17 @@
+//
+//  TakeMyPicApp.swift
+//  TakeMyPic
+//
+//  Created by Howard Gil on 6/4/26.
+//
+
+import SwiftUI
+
+@main
+struct TakeMyPicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
